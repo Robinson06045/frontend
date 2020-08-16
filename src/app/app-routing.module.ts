7297b4eba9a79b395d34e23622a4cb0cbaf5741b
Robinson Ruilova <robinjosellove@gmail.com>
2020-08-16 14:17:38 -0500

    fronted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BaseComponent } from './components/base/base.component';
+import { DosComponent } from './components/dos/dos.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { TurnComponent } from './components/turn/turn.component';
 
-const routes: Routes = [];
+const routes: Routes = [ //cre aqui las rutas
+  {
+    path:'',//direccion de la ruta
+    component:BaseComponent
+  },
+  {
+    path:'dos',
+    component:DosComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'contact',
+    component:ContactComponent
+  },
+  {
+    path:'cars',
+    component:CarsComponent
+  },
+  {
+    path:'turn',
+    component:TurnComponent
+  }
+  //por cada compoenente se crea su importacion
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BaseComponent } from './components/base/base.component';
+//importacion de angular-material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { DosComponent } from './components/dos/dos.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { RegisterComponent } from './components/register/register.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { TurnComponent } from './components/turn/turn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    DosComponent,
+    RegisterComponent,
+    ContactComponent,
+    CarsComponent,
+    TurnComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, // aqui va lo que importe
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
